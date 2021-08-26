f450392f9b9fa72aaee488686896ec83032e0a94
fkingBug <fkingbug@gmail.com>
2021-08-26 18:05:14 +0300

    BestInterface

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,34 +1,10 @@
-import { WeatherProps } from './../types/weather'
+import { ActionTypes, initialStateProps, ActionEnum } from '../types/weather'
 
-export enum ActionEnum {
-  FETCH_WEATHER = 'FETCH_WEATHER',
-  FETCH_WEATHER_SUCCESS = 'FETCH_WEATHER_SUCCESS',
-  FETCH_WEATHER_ERROR = 'FETCH_WEATHER_ERROR',
-}
-
-interface actionWeatherFetch {
-  type: ActionEnum.FETCH_WEATHER
-}
-interface actionWeatherFetchSuccess {
-  type: ActionEnum.FETCH_WEATHER_SUCCESS
-  payload: any
-}
-interface actionWeatherFetchError {
-  type: ActionEnum.FETCH_WEATHER_ERROR
-  payload: string
-}
-interface initialStateProps {
-  weather: WeatherProps | null
-  isLoading: boolean
-  error: string | null
-}
 const initialState: initialStateProps = {
   weather: null,
   isLoading: false,
   error: null,
 }
-
-export type ActionTypes = actionWeatherFetch | actionWeatherFetchSuccess | actionWeatherFetchError
 
 export const rootReducer = (state = initialState, action: ActionTypes): initialStateProps => {
   switch (action.type) {
--- src/types/weather.ts
+++ src/types/weather.ts
@@ -1,3 +1,27 @@
+import { WeatherProps } from './../types/weather'
+
+export enum ActionEnum {
+  FETCH_WEATHER = 'FETCH_WEATHER',
+  FETCH_WEATHER_SUCCESS = 'FETCH_WEATHER_SUCCESS',
+  FETCH_WEATHER_ERROR = 'FETCH_WEATHER_ERROR',
+}
+
+interface actionWeatherFetch {
+  type: ActionEnum.FETCH_WEATHER
+}
+interface actionWeatherFetchSuccess {
+  type: ActionEnum.FETCH_WEATHER_SUCCESS
+  payload: any
+}
+interface actionWeatherFetchError {
+  type: ActionEnum.FETCH_WEATHER_ERROR
+  payload: string
+}
+interface initialStateProps {
+  weather: WeatherProps | null
+  isLoading: boolean
+  error: string | null
+}
 export interface WeatherProps {
   name: string
   weather: skyProp[]
